4d1413a19c79a0d6a7a75db99af4a8482f492f7e
Kriegslok <niors@mail.ru>
2021-02-16 12:51:32 +0300

    SimpleRequest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class Main {
+    static Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
+        SimpleRequest sr = new SimpleRequest();
+        sr.init();
+
+
         System.out.println("111");
+        logger.info("1111");
+
     }
 }
