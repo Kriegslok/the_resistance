0625e3c26d2a52a7f628afef111afb3bb5270635
Kriegslok <niors@mail.ru>
2021-02-18 13:25:03 +0300

    Example from TDLib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
+
     static Logger logger = LoggerFactory.getLogger(Main.class.getName());
 
-    public static void main(String[] args) {
-        SimpleRequest sr = new SimpleRequest();
-        sr.init();
+
+    public static void main(String[] args) throws InterruptedException {
+//        SimpleRequest sr = new SimpleRequest();
+//        sr.init();
+        RClient rClient = new RClient();
+        rClient.init();
 
 
-        System.out.println("111");
-        logger.info("1111");
+//        System.out.println("111");
+//        logger.info("1111");
 
     }
 }
