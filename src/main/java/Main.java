514c4b7044ce2be78322eca543a285a764d16b8b
Kriegslok <niors@mail.ru>
2021-03-01 14:42:09 +0300

    Getting updates via asyncHttpClient

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,20 @@
 
 
-import org.json.simple.parser.ParseException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 public class Main {
 
     static Logger logger = LoggerFactory.getLogger(Main.class.getName());
 
 
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, ParseException {
+    public static void main(String[] args) {
         RClient client = new RClient();
         client.getUpdates();
+        client.printMessages();
 
-
-
-//        System.out.println("111");
-//        logger.info("1111");
 
     }
 }
