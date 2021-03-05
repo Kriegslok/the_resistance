8dbb374c3731b8e7d00dc30f7cd3bc9f07b5c3cb
Kriegslok <niors@mail.ru>
2021-03-05 16:29:01 +0300

    Getting updates via asyncHttpClient

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 
 
 
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +10,18 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
     static Logger logger = LoggerFactory.getLogger(Main.class.getName());
+    static String telegramBotUrl = "https://api.telegram.org/";
+    static String token = "bot1535167887:AAHspHwJURI66rdMqxim_xYxjMGrKQWngKk/";
 
 
     public static void main(String[] args) {
-        RClient client = new RClient();
+        RClientConfig config = new RClientConfig(telegramBotUrl, token);
+        AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient();
+        RClient client = new RClient(asyncHttpClient, config);
         client.getUpdates();
-        client.printMessages();
+        //client.printMessages();
         client.getUpdates();
-        client.printMessages();
+        //client.printMessages();
 
 
     }
